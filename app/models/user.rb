2f34a91a0d728d2a5edb100783fb82c5e34fe76c
Ryan Storberg <ryan@storberg.net>
2015-02-04 12:38:18 -0500

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save :confirm_avatar
+
   has_many :active_relationships,  class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
@@ -11,6 +13,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_attached_file :avatar, :styles => { :small => "30x30#", :medium => "60x60#", :large => "180x180#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   def knowledge_ratio
     total_pages = books.sum(:pages)
@@ -32,4 +35,10 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following.include?(other_user)
   end
+
+  # def confirm_avatar
+  #   unless avatar
+  #     avatar = ActionController::Base.helpers.asset_path('eh.png')
+  #   end
+  # end
 end
